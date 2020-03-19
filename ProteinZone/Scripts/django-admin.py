c0ab727e47fa8dc0fc1c48a5215ae99799491266
Pushkar A <asapure.pushkar.004@gmail.com>
2020-03-19 16:24:27 +0530

    Created basic project structure and Supplement model

---
+++ ProteinZone/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!f:\git\proteinzone\proteinzone\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ Shop/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.utils import timezone
+from django.conf import settings
+
+# Create your models here.
+class Supplement(models.Model):
+    name = models.CharField(max_length=200,unique=True, blank=False, null=False)
+    supplementType = models.CharField(max_length=50, blank=False, null=False)
+    image = models.ImageField(upload_to='supplements/')
+    details = models.CharField(max_length=300)
+    createdOn = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
